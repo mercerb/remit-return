a10a792c29ce4a79a666238177f535940c0f0daa
ahsanimran98 <ahsanimran1997@gmail.com>
2023-05-02 22:19:12 -0400

    Updates

--- _app/immutable/chunks/1.eee5bf49.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.c7459d9b.js";export{t as component};
--- _app/immutable/chunks/2.761e9f1e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.6cdeb626.js";export{t as component};
